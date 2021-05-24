4a176bd229549bddbb3dd7ed94b3a90221855196
Руслан Алимов <rusichviz@gmail.com>
2021-05-24 13:47:07 +0300

    requirements

--- polls/api/urls.py
+++ polls/api/urls.py
@@ -3,8 +3,8 @@ from .views import (PollViewSet, QuestionViewSet, ChoiceViewSet,
                     AnswerViewSet, UserAnswersListView, UserPollsListView)
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.schemas import get_schema_view
 from django.views.generic import TemplateView
+
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, basename='polls')
@@ -28,11 +28,6 @@ urlpatterns = [
     path('user-answers/<int:user_id>/', UserPollsListView.as_view()),
     path('user-answers/<int:user_id>/<int:poll_id>/',
          UserAnswersListView.as_view()),
-    path('schema', get_schema_view(
-        title="PollsAPI",
-        description="API for polls",
-        version="1.0.0"
-    ), name='openapi-schema'),
     path('redoc/', TemplateView.as_view(template_name='redoc.html'),
          name='redoc'),
 ]
